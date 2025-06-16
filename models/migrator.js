0809ad67c7e2c6f29232f6b5a54fd7a4d75d001e
Carlos Dourado <carlosismaelad@outlook.com>
2025-06-15 23:41:40 -0300

    feat: hide migrator logs

--- models/migrator.js
+++ models/migrator.js
@@ -1,12 +1,13 @@
 import { resolve } from "node:path";
 import migrationRunner from "node-pg-migrate";
 import database from "infra/database.js";
+import { log } from "node:console";
 
 const defaultMigrationsOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
